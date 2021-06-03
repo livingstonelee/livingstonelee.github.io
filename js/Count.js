fec8a2275b6a09d78d8b2e3289f8cb9a9bb41b9b
Yuchen LEE <livingstonelee@outlook.com>
2021-06-03 19:19:28 +0800

    Update Count.js

--- js/Count.js
+++ js/Count.js
@@ -9,9 +9,33 @@ function Count() {
     this.second = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
 
 	this.nextmonth = 6;
-	this.nextdate = 12;
+	this.nextdate = 13;
+	this.nexthour = 17;
+	this.nextmin = 25;
+	this.nextsec = 0;
 	
-	this.totalday = (this.nextmonth*30 + this.nextdate) - (this.month*30 + this.date);
+	this.nexttotal = ((((this.nextmonth)*30 + this.nextdate)*24 + this.nexthour)*60 + this.nextmin)*60 + this.nextsec;
+	this.nowtotal = ((((this.month)*30 + this.date)*24 + this.hour)*60 + this.minute)*60 + this.second;
+	this.total = this.nexttotal - this.nowtotal;
+		
+	int waitdate = this.total / (60*60*24);
+	int waithour = (this.total - waitdate*60*60*24) / (60*60);
+	int waitmin = (this.total - waitdate*60*60*24 - waithour*60*60) / 60;
+	int waitsec = this.total - waitdate*60*60*24 - waithour*60*60 - waitmin*60;
+	
+	
+		
+	}
+	else{
+		this.waitday = (this.nextmonth-this.month)*30 + (this.nextdate-this.date) -1 ;
+		
+		
+		
+		
+		
+	}
+	
+	
 
 
 
@@ -28,7 +52,7 @@ function Count() {
     };
     
 	this.toCount = function() {
-        return "距离下一次相遇还有 : " + " " +this.totalday + " Day " + this.hour + ":" + this.minute + ":" + (60-this.second); 
+        return "距离下一次相遇还有 : " + " " + waitdate + " Day " + waithour + ":" + waitmin + ":" + waitsec; 
     };
 	
 	
